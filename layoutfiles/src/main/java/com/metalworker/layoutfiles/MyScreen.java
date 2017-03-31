d363de5e756d75fd7bbc275c3b9c34c6d6d0051c
SamMetalWorker <dragongling@gmail.com>
2017-03-31 10:03:04 +0500

    Lesson 5 passed!

---
+++ layoutfiles/src/main/java/com/metalworker/layoutfiles/MyScreen.java
@@ -0,0 +1,13 @@
+package com.metalworker.layoutfiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_screen);
+    }
+}
